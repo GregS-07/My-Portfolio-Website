0a9024b6f3c45d2fd0b1335f4aee1410701f54cf
Grzegorz Sternik <131522817+GregS-07@users.noreply.github.com>
2024-05-29 11:01:05 +0000

    Type Writing Effect For Profile

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+var i = 0;
+var txt = 'I am a hardworking College student and intern. I practice programming in College, my industry placement and in my spare time. I am an adaptable, quick learner who takes pride in all my work. I am looking for entry level work with a preference for web development but I’m open to other areas.';
+var speed = 50;
+
 $(document).ready(() => {
+
+  function typeWriter() {
+    if (i < txt.length) {
+      $('.profile').append(txt.charAt(i));
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+  typeWriter();
 
   //toggle fixed contact popup
   $(".fixed-title").on("click", () => {
